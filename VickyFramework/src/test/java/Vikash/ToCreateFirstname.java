36967c1417f880ac62a7bb799b9d53c8a83ebcaa
Neha Singh <Neha Singh@DESKTOP-UQAESM6.mshome.net>
2024-11-05 09:16:46 +0530

    2nd push

---
+++ VickyFramework/src/test/java/Vikash/ToCreateFirstname.java
@@ -0,0 +1,10 @@
+package Vikash;
+
+public class ToCreateFirstname {
+
+	public static void main(String[] args) {
+		System.out.println("Firstname created successfully");
+
+	}
+
+}
---
+++ VickyFramework/src/test/java/singh/ToCreateLastname.java
@@ -0,0 +1,10 @@
+package singh;
+
+public class ToCreateLastname {
+
+	public static void main(String[] args) {
+		System.out.println("lastname is created");
+
+	}
+
+}
