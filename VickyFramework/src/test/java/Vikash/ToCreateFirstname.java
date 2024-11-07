490b1b274e413ac8520f08e06079c864bc93a774
samee <samee@DESKTOP-4VL1BVC>
2024-11-07 15:49:20 +0530

    i have booked tickets

--- VickyFramework/src/test/java/Vikash/ToCreateFirstname.java
+++ VickyFramework/src/test/java/Vikash/ToCreateFirstname.java
@@ -3,7 +3,7 @@ package Vikash;
 public class ToCreateFirstname {
 
 	public static void main(String[] args) {
-		//System.out.println("Firstname created successfully");
+		System.out.println("Firstname created successfully");
 
 	}
 
---
+++ VickyFramework/src/test/java/calendar/BookTciketsForPinky.java
@@ -0,0 +1,8 @@
+package calendar;
+
+public class BookTciketsForPinky {
+	public static void main(String[] args) {
+		System.out.println("i have booked tickets for pinky");
+	}
+
+}
